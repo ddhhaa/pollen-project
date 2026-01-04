3c823ce1be53703f3c082e912e7ec1fb3e0c5384
Дарья Ступина <dstupina00@gmail.com>
2026-01-04 20:58:04 +0500

    Initial Django project with models

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+class Location(models.Model):
+    pass
+
+class UserPollenRisk(models.Model):
+    pass
+
+class Recommendation(models.Model):
+    pass
