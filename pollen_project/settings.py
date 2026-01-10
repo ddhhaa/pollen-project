93e660c025b9624b2a08e6011aba05265fca2234
Дарья Ступина <dstupina00@gmail.com>
2026-01-11 01:19:18 +0500

    ready for deploy

--- pollen_project/settings.py
+++ pollen_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-8bbh%!&v_m24r2j7u_rfr#+0n(&4((9f#$&(-joq_1i*29ug_8
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'ddhhaa.pythonanywhere.com'
+    'ddhhaa.pythonanywhere.com', '127.0.0.1'
 ]
 
 
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
