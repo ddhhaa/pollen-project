7c40da0a81f524c390e4a9f8ce6176b62704be08
Дарья Ступина <dstupina00@gmail.com>
2026-01-12 00:57:06 +0500

    add user authentication and profile page

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import home, register
+from .views import home, register, login_view, logout_view, profile
 
 urlpatterns = [
     path('', home, name='home'),
     path("register/", register, name="register"),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('profile/', profile, name='profile'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,13 @@
+# views.py - правильные импорты
 from django.shortcuts import render, redirect
-from .models import PollenData, UserProfile
+from .models import PollenData, UserProfile, PollenType, UserAllergy
 from .open_meteo import fetch_pollen_data
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def home(request):
 
     period = request.GET.get('period', 'day')
@@ -50,3 +54,48 @@ def register(request):
         form = RegisterForm()
 
     return render(request, "register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = AuthenticationForm()
+    return render(request, "login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
+
+@login_required
+def profile(request):
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+    
+    # Получаем аллергии пользователя
+    user_allergies = user_profile.allergies.all()
+    
+    # Получаем все типы пыльцы
+    all_pollen_types = PollenType.objects.all()
+    
+    if request.method == "POST":
+        # Обработка выбора аллергенов
+        selected_ids = request.POST.getlist('allergies')
+        user_profile.allergens.clear()  # Очищаем старые
+        
+        for pollen_id in selected_ids:
+            pollen_type = PollenType.objects.get(id=pollen_id)
+            UserAllergy.objects.create(
+                user=user_profile,
+                pollen_type=pollen_type,
+                sensitivity=3
+            )
+        return redirect('profile')
+    
+    return render(request, 'profile.html', {
+        'user_profile': user_profile,
+        'all_pollen': all_pollen_types,
+        'user_allergies': user_allergies,
+    })
