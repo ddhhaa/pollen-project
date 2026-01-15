2d81d7f9efc17b4a10263ff0cf7494be1998f44b
Дарья Ступина <dstupina00@gmail.com>
2026-01-15 20:59:40 +0500

    change start page

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import home, register, login_view, logout_view, profile
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', login_view, name='login'),
     path("register/", register, name="register"),
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
+    path('home/', login_required(home, login_url='/login/'), name='home'),
     path('profile/', profile, name='profile'),
 ]
